9e08f26844f7ae241ec9cbdd9ebf056b16a0b8bd
Florian Collip <florian.collip@stud.hshl.de>
2022-08-02 00:54:06 +0200

    Add responsive behaviour

--- src/assets/theme/theme.js
+++ src/assets/theme/theme.js
@@ -9,6 +9,26 @@ const theme = extendTheme({
       },
     }),
   },
+  components: {
+    Menu: {
+      baseStyle: props => ({
+        list: {
+          rounded: 'none',
+          background: props.colorMode === 'dark' ? '#060606' : 'white',
+          borderColor:
+            props.colorMode === 'dark' ? 'whiteAlpha.300' : 'blackAlpha.300',
+        },
+        item: {
+          textTransform: 'uppercase',
+          letterSpacing: '0.1em',
+          _hover: {
+            background:
+              props.colorMode === 'dark' ? 'whiteAlpha.100' : 'blackAlpha.100',
+          },
+        },
+      }),
+    },
+  },
   semanticTokens: {
     shadows: {
       glow: {
--- src/components/Header/HeaderTab.js
+++ src/components/Header/HeaderTab.js
@@ -1,126 +1,135 @@
 import {
   Box,
-  HStack,
+  Stack,
   Icon,
   Text,
   Spacer,
   useColorMode,
   useTheme,
+  forwardRef,
 } from '@chakra-ui/react';
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useLocation, Link } from 'react-router-dom';
 
-const HeaderTab = ({
-  route,
-  text,
-  TabIcon,
-  onClick,
-  indicatorRef,
-  isSpacer,
-}) => {
-  const theme = useTheme();
-  const { colorMode } = useColorMode();
-  const tabElement = useRef(null);
+const HeaderTab = forwardRef(
+  ({ route, text, TabIcon, onClick, indicatorRef, isSpacer }, ref) => {
+    const theme = useTheme();
+    const { colorMode } = useColorMode();
+    const tabElement = useRef(null);
 
-  const [dimensions, setDimensions] = useState({
-    height: window.innerHeight,
-    width: window.innerWidth,
-  });
+    const [dimensions, setDimensions] = useState({
+      height: window.innerHeight,
+      width: window.innerWidth,
+    });
 
-  const { pathname } = useLocation();
+    const { pathname } = useLocation();
 
-  useEffect(() => {
-    if (
-      pathname === route &&
-      tabElement.current !== null &&
-      indicatorRef.current !== null
-    ) {
-      let prevValue = JSON.stringify(
-        tabElement.current.getBoundingClientRect()
-      );
+    useEffect(() => {
+      if (
+        pathname === route &&
+        tabElement.current !== null &&
+        indicatorRef.current !== null
+      ) {
+        let prevValue = JSON.stringify(
+          tabElement.current.getBoundingClientRect()
+        );
 
-      const handle = setInterval(() => {
-        if (tabElement.current === null) {
-          clearInterval(handle);
-        } else {
-          const nextValue = JSON.stringify(
-            tabElement.current.getBoundingClientRect()
-          );
-
-          if (nextValue === prevValue) {
-            indicatorRef.current.setAttribute(
-              'style',
-              `width: ${tabElement.current.getBoundingClientRect().width}px;
-                left: ${tabElement.current.getBoundingClientRect().left}px;`
-            );
-
+        const handle = setInterval(() => {
+          if (tabElement.current === null) {
             clearInterval(handle);
           } else {
-            prevValue = nextValue;
+            const nextValue = JSON.stringify(
+              tabElement.current.getBoundingClientRect()
+            );
+
+            if (nextValue === prevValue) {
+              indicatorRef.current.setAttribute(
+                'style',
+                `width: ${tabElement.current.getBoundingClientRect().width}px;
+                left: ${tabElement.current.getBoundingClientRect().left}px;`
+              );
+
+              clearInterval(handle);
+            } else {
+              prevValue = nextValue;
+            }
+          }
+        }, 100);
+      }
+    }, [indicatorRef, tabElement, dimensions, pathname, route]);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setDimensions({
+          height: window.innerHeight,
+          width: window.innerWidth,
+        });
+      };
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    });
+
+    const content = (
+      <Stack
+        ref={ref}
+        align="center"
+        justify="center"
+        direction={{ base: 'column', xl: 'row' }}
+        _hover={{
+          bg: colorMode === 'dark' ? 'whiteAlpha.100' : 'blackAlpha.100',
+          transform: { base: 'translateY(0px)', md: 'translateY(-2px)' },
+        }}
+        transform={
+          pathname === route && {
+            base: 'translateY(0px)',
+            md: 'translateY(-2px)',
           }
         }
-      }, 100);
+        bg={pathname === route ? 'whiteAlpha.200' : null}
+        transition="background-color 150ms ease-in-out, transform 150ms ease-in-out"
+        spacing={{ base: 1, xl: 4 }}
+        h="full"
+        px={text !== '' ? { base: 2, md: 4, xl: 8 } : { base: 1, md: 2, xl: 4 }}
+      >
+        <Icon
+          boxSize={{ base: 5, xl: 6 }}
+          as={TabIcon}
+          filter={colorMode === 'dark' ? theme.shadows.dropGlow : 'none'}
+        />
+        {text !== '' ? (
+          <Text
+            textShadow="glow"
+            fontSize={{ base: 'xs', md: 'md', xl: 'xl' }}
+            letterSpacing="0.1em"
+          >
+            {text}
+          </Text>
+        ) : null}
+      </Stack>
+    );
+
+    if (isSpacer) {
+      return <Spacer />;
     }
-  }, [indicatorRef, tabElement, dimensions, pathname, route]);
 
-  React.useEffect(() => {
-    const handleResize = () => {
-      setDimensions({
-        height: window.innerHeight,
-        width: window.innerWidth,
-      });
-    };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  });
+    if (route !== '') {
+      return (
+        <Link ref={tabElement} style={{ height: '100%' }} to={route}>
+          {content}
+        </Link>
+      );
+    }
 
-  const content = (
-    <HStack
-      _hover={{
-        bg: colorMode === 'dark' ? 'whiteAlpha.100' : 'blackAlpha.100',
-        transform: 'translateY(-2px)',
-      }}
-      transform={pathname === route && 'translateY(-2px)'}
-      bg={pathname === route ? 'whiteAlpha.200' : null}
-      transition="background-color 150ms ease-in-out, transform 150ms ease-in-out"
-      spacing={4}
-      h="full"
-      px={text !== '' ? 8 : 4}
-    >
-      <Icon
-        boxSize={6}
-        as={TabIcon}
-        filter={colorMode === 'dark' ? theme.shadows.dropGlow : 'none'}
-      />
-      {text !== '' ? (
-        <Text textShadow="glow" fontSize="xl" letterSpacing="0.1em">
-          {text}
-        </Text>
-      ) : null}
-    </HStack>
-  );
-
-  if (isSpacer) {
-    return <Spacer />;
-  }
-
-  if (route !== '') {
     return (
-      <Link ref={tabElement} style={{ height: '100%' }} to={route}>
+      <Box cursor="pointer" onClick={onClick} h="full">
         {content}
-      </Link>
+      </Box>
     );
   }
-
-  return (
-    <Box cursor="pointer" onClick={onClick} h="full">
-      {content}
-    </Box>
-  );
-};
+);
 
 HeaderTab.propTypes = {
   route: PropTypes.string,
