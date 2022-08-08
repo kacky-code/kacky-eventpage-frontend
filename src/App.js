9d9b6fb7d2566fe0e07dd167af8695181c248b32
Florian Collip <florian.collip@stud.hshl.de>
2022-08-08 02:41:55 +0200

    Implement Dashboard

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { Box } from '@chakra-ui/react';
 import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header/Header';
+import Dashboard from './views/Dashboard/Dashboard';
 
 const App = () => (
   <Box textAlign="center">
     <Routes>
       <Route path="/" element={<Header>Header</Header>}>
-        <Route index element={<div>Dashboard</div>} />
+        <Route index element={<Dashboard />} />
         <Route path="spreadsheet" element={<div>Spreadsheet</div>} />
         <Route path="leaderboard" element={<div>Leaderboard</div>} />
 
