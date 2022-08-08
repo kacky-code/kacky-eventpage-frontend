b956087c14bd1625e224a6c5ba44646c39624fb8
Florian Collip <florian.collip@stud.hshl.de>
2022-08-08 02:37:34 +0200

    Import all font weights

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { ColorModeScript, ChakraProvider } from '@chakra-ui/react';
 import theme from './assets/theme/theme';
+import '@fontsource/oswald/200.css';
 import '@fontsource/oswald/300.css';
+import '@fontsource/oswald/400.css';
+import '@fontsource/oswald/500.css';
+import '@fontsource/oswald/600.css';
+import '@fontsource/oswald/700.css';
 
 import App from './App';
 
