992b12d316612de0f11797a8946d3849870c3ca1
Florian Collip <florian.collip@stud.hshl.de>
2022-08-20 20:17:03 +0200

    Implement Login and Register

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-constructed-context-values */
+import React, { useState } from 'react';
 import { Box } from '@chakra-ui/react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Routes, Route } from 'react-router-dom';
 
+import Cookies from 'universal-cookie';
 import Header from './components/Header/Header';
 import Dashboard from './views/Dashboard/Dashboard';
 import SpreadSheet from './views/Spreadsheet/Spreadsheet';
 import Profile from './views/Profile/Profile';
+import AuthContext from './context/AuthContext';
+
+const cookies = new Cookies();
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <Box textAlign="center">
-      <Routes>
-        <Route path="/" element={<Header>Header</Header>}>
-          <Route index element={<Dashboard />} />
-          <Route path="spreadsheet" element={<SpreadSheet />} />
-          <Route path="leaderboard" element={<div>Leaderboard</div>} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="*" element={<div>Nothing here</div>} />
-        </Route>
-      </Routes>
-    </Box>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
-);
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(() => cookies.get('token') || '');
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthContext.Provider
+        value={{ isLoggedIn, setIsLoggedIn, token, setToken }}
+      >
+        <Box textAlign="center">
+          <Routes>
+            <Route path="/" element={<Header>Header</Header>}>
+              <Route index element={<Dashboard />} />
+              <Route path="spreadsheet" element={<SpreadSheet />} />
+              <Route path="leaderboard" element={<div>Leaderboard</div>} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="*" element={<div>Nothing here</div>} />
+            </Route>
+          </Routes>
+        </Box>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </AuthContext.Provider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
--- src/components/Header/AuthModal/Register.js
+++ src/components/Header/AuthModal/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import {
   Text,
   FormControl,
@@ -7,47 +7,226 @@ import {
   Button,
   VStack,
   Divider,
+  Flex,
   FormHelperText,
+  FormErrorMessage,
+  HStack,
 } from '@chakra-ui/react';
 
 import PropTypes from 'prop-types';
 
+import { useForm } from 'react-hook-form';
+
+import { useMutation } from '@tanstack/react-query';
+import { registerUser } from '../../../api/api';
+
 // eslint-disable-next-line no-unused-vars, arrow-body-style
 const Register = ({ setMode }) => {
-  return (
-    <>
-      <Text textShadow="glow" letterSpacing="0.1em" fontSize="xl">
-        Register
-      </Text>
-      <VStack spacing={6} w="full">
-        <FormControl>
-          <FormLabel>Your E-Mail</FormLabel>
-          <Input placeholder="E-Mail" />
-          <FormHelperText>
-            We store your e-mail adress in a hash, that means we don&apos;t
-            actually know your adress. No spam for you.
-          </FormHelperText>
-        </FormControl>
-        <FormControl>
-          <FormLabel>Your Username</FormLabel>
-          <Input placeholder="Password" />
-          <FormHelperText>
-            This does not have to be your ingame name.
-          </FormHelperText>
-        </FormControl>
-        <Divider />
-        <FormControl>
-          <FormLabel>Your Password</FormLabel>
-          <Input placeholder="Password" />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Confirm Password</FormLabel>
-          <Input placeholder="Password" />
-        </FormControl>
-      </VStack>
+  const [isRegistered, setIsRegistered] = useState(false);
 
-      <Button w="full">Register</Button>
-    </>
+  const [serverError, setServerError] = useState({
+    isError: false,
+    message: '',
+  });
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    watch,
+  } = useForm();
+
+  const password = useRef({});
+  password.current = watch('password', '');
+
+  const mutation = useMutation(
+    data => registerUser(data.username, data.password, data.mailadress),
+    {
+      onSuccess: () => {
+        setIsRegistered(true);
+        console.log('SUCCESS');
+      },
+      onError: () => {
+        setServerError({
+          isError: true,
+          message: 'Username or E-Mail already taken',
+        });
+        console.log('ERROR');
+        console.log(mutation);
+      },
+    }
+  );
+
+  const onSubmit = data => mutation.mutate(data);
+
+  return (
+    <Flex
+      direction="column"
+      w="full"
+      h="full"
+      align="center"
+      justify="space-around"
+      as="form"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      {isRegistered ? (
+        <HStack justify="center" spacing={4} w="full">
+          <Text letterSpacing="0.1em" fontSize="sm" textShadow="glow">
+            Successfully registered!
+          </Text>
+          <Text
+            letterSpacing="0.1em"
+            textDecoration="underline"
+            color="blue.100"
+            fontSize="sm"
+            textShadow="glow"
+            _hover={{ color: 'blue.200', cursor: 'pointer' }}
+            onClick={() => setMode('register')}
+          >
+            Login now
+          </Text>
+        </HStack>
+      ) : (
+        <>
+          <Text textShadow="glow" letterSpacing="0.1em" fontSize="xl">
+            Register
+          </Text>
+          <VStack spacing={6} w="full">
+            <FormControl isInvalid={errors.mailadress}>
+              <FormLabel>E-Mail</FormLabel>
+              <Input
+                {...register('mailadress', {
+                  required: 'E-Mail is required',
+                  minLength: {
+                    value: 4,
+                    message: 'Minimum length should be 4',
+                  },
+                  maxLength: {
+                    value: 80,
+                    message: 'Really? Sleeping on your Keyboard?',
+                  },
+                  pattern: {
+                    value: /[a-z0-9]*@[a-z0-9]*\.[a-z]*/,
+                    message: 'Not a valid mail adress',
+                  },
+                })}
+                type="email"
+                id="mailadress"
+                placeholder="E-Mail"
+              />
+              {!errors.mailadress ? (
+                <FormHelperText>Enter your E-Mail here</FormHelperText>
+              ) : (
+                <FormErrorMessage>
+                  {errors.mailadress ? errors.mailadress.message : null}
+                </FormErrorMessage>
+              )}
+            </FormControl>
+            <FormControl isInvalid={errors.username}>
+              <FormLabel>Username</FormLabel>
+              <Input
+                {...register('username', {
+                  required: 'Username is required',
+                  minLength: {
+                    value: 4,
+                    message: 'Minimum length should be 4',
+                  },
+                  maxLength: {
+                    value: 80,
+                    message: 'Really? Sleeping on your Keyboard?',
+                  },
+                  pattern: {
+                    value: /[a-zA-Z0-9]*/,
+                    message: 'Only letters and numbers allowed',
+                  },
+                })}
+                id="username"
+                placeholder="Username"
+              />
+              {!errors.username ? (
+                <FormHelperText>Enter your username here</FormHelperText>
+              ) : (
+                <FormErrorMessage>
+                  {errors.username ? errors.username.message : null}
+                </FormErrorMessage>
+              )}
+            </FormControl>
+            <Divider />
+            <FormControl isInvalid={errors.password}>
+              <FormLabel>Your Password</FormLabel>
+              <Input
+                {...register('password', {
+                  required: 'You must specify a password',
+                  minLength: {
+                    value: 8,
+                    message: 'Minimum length should be 8',
+                  },
+                  maxLength: {
+                    value: 80,
+                    message: 'Really? Sleeping on your Keyboard?',
+                  },
+                })}
+                id="password"
+                type="password"
+                placeholder="Password"
+              />
+              {!errors.password ? (
+                <FormHelperText>Enter your password here</FormHelperText>
+              ) : (
+                <FormErrorMessage>
+                  {errors.password ? errors.password.message : null}
+                </FormErrorMessage>
+              )}
+            </FormControl>
+            <FormControl isInvalid={errors.passwordConfirm}>
+              <FormLabel>Confirm Password</FormLabel>
+              <Input
+                {...register('passwordConfirm', {
+                  required: 'Password is required',
+                  minLength: {
+                    value: 8,
+                    message: 'Minimum length should be 8',
+                  },
+                  maxLength: {
+                    value: 80,
+                    message: 'Really? Sleeping on your Keyboard?',
+                  },
+                  validate: value =>
+                    value === password.current || 'The passwords do not match',
+                })}
+                id="passwordConfirm"
+                type="password"
+                placeholder="Password"
+              />
+              {!errors.passwordConfirm ? (
+                <FormHelperText>Confirm your password here</FormHelperText>
+              ) : (
+                <FormErrorMessage>
+                  {errors.passwordConfirm
+                    ? errors.passwordConfirm.message
+                    : null}
+                </FormErrorMessage>
+              )}
+            </FormControl>
+          </VStack>
+          <VStack spacing={4} w="full">
+            <Text
+              h="21px"
+              color="red.500"
+              _dark={{ color: 'red.300' }}
+              fontWeight="normal"
+              fontSize="sm"
+              letterSpacing="0.1em"
+            >
+              {serverError.isError ? serverError.message : null}
+            </Text>
+            <Button w="full" type="submit">
+              Register
+            </Button>
+          </VStack>
+        </>
+      )}
+    </Flex>
   );
 };
 
